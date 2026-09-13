2959d3b1767da293bfaebb5ebc99b3b46a7db9a9
HIRORORO\user <hiro.0274417@gmail.com>
2026-09-14 02:32:24 +0900

    ロゴ入れ

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -8,6 +8,7 @@
         <title>AtlasBulletinBoard</title>
 
         <!-- Fonts -->
+
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
         <link href="{{ asset('css/style.css') }}" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
