c8933e14781f0db56f7b71c29e3af6093af4bf68
HIRORORO\user <hiro.0274417@gmail.com>
2026-09-17 00:12:42 +0900

    トップ画面修正

--- resources/views/auth/login/login.blade.php
+++ resources/views/auth/login/login.blade.php
@@ -2,7 +2,7 @@
   <form action="{{ route('loginPost') }}" method="POST">
     <div class="all_content w-100 vh-100 d-flex flex-column" style="align-items:center; justify-content:center;">
       <img src="{{ asset('image/atlas-black.png') }}" alt="Atlas" width="200" class="mb-5">
-      <div class="border vh-50 w-25 bg-white login_form" >
+      <div class="border vh-50  bg-white login_form" >
         <div class="w-75 m-auto pt-5 ">
           <label class="d-block m-0" style="font-size:13px;">メールアドレス</label>
           <div class="border-bottom border-primary w-100">
@@ -18,7 +18,7 @@
         <div class="text-right m-3">
           <input type="submit" class="btn btn-primary" value="ログイン">
         </div>
-        <div class="text-center">
+        <div class="text-center mb-4">
           <a href="{{ route('registerView') }}">新規登録はこちら</a>
         </div>
       </div>
