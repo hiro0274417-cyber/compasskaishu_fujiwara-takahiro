d6b405ed3f3e2bc081e36b2276f705f3c6bb4b96
HIRORORO\user <hiro.0274417@gmail.com>
2026-09-16 23:52:14 +0900

    フォームのフォルム変更

--- resources/views/auth/login/login.blade.php
+++ resources/views/auth/login/login.blade.php
@@ -2,8 +2,8 @@
   <form action="{{ route('loginPost') }}" method="POST">
     <div class="all_content w-100 vh-100 d-flex flex-column" style="align-items:center; justify-content:center;">
       <img src="{{ asset('image/atlas-black.png') }}" alt="Atlas" width="200" class="mb-5">
-      <div class="border vh-50 w-25">
-        <div class="w-75 m-auto pt-5">
+      <div class="border vh-50 w-25 bg-white login_form" >
+        <div class="w-75 m-auto pt-5 ">
           <label class="d-block m-0" style="font-size:13px;">メールアドレス</label>
           <div class="border-bottom border-primary w-100">
             <input type="text" class="w-100 border-0" name="mail_address">
