60985749d5edf484e9393f693a30136238a34529
HIRORORO\user <hiro.0274417@gmail.com>
2026-09-24 21:52:07 +0900

    初期データの登録完了

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -3,6 +3,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +14,19 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+            'over_name' => 'テスト',
+            'under_name' => '太郎',
+            'over_name_kana' => 'テスト',
+            'under_name_kana' => 'タロウ',
+            'mail_address' => 'test@example.com',
+            'sex' => 1,
+            'birth_day' => '2000-01-01',
+            'role' => 1,
+            'password' => bcrypt('password'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
 
     }
 }
